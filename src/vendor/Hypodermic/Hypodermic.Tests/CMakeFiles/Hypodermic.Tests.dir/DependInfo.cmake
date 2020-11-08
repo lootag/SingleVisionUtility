b576b2f94b3661c8b28130f64a683979d0abbe0c
lootag <luigi.taglialatela1996@gmail.com>
2020-11-08 10:12:50 +0100

    making src/vendor

---
+++ src/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CircularDependencyTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/CircularDependencyTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/ContainerBuilderTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/ContainerBuilderTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/ContainerTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/ContainerTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/DefaultConstructibleTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/DefaultConstructibleTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/FactoryTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/FactoryTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/IsCompleteTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/IsCompleteTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/MemoryTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/MemoryTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/NamedTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/NamedTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/NestedContainerTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/NestedContainerTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/PerformanceTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/PerformanceTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/PersistentInstanceRegistrationTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/PersistentInstanceRegistrationTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/ProvidedDependenciesTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/ProvidedDependenciesTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/ProvidedInstanceFactoryRegistrationTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/ProvidedInstanceFactoryRegistrationTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/ProvidedInstanceRegistrationTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/ProvidedInstanceRegistrationTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/RegistrationTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/RegistrationTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/RuntimeRegistrationTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/RuntimeRegistrationTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/UseIfNoneTests.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/UseIfNoneTests.cpp.o"
+  "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/main.cpp" "/home/alex/repos/SingleVisionUtility/bin/lib/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_TEST_DYN_LINK"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "lib/vendor/Hypodermic/Hypodermic.Tests/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/vendor/Hypodermic/Hypodermic.Tests/CMakeFiles/Hypodermic.Tests.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hypodermic.Tests.dir/CircularDependencyTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/ContainerBuilderTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/ContainerTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/DefaultConstructibleTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/FactoryTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/IsCompleteTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/MemoryTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/NamedTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/NestedContainerTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/PerformanceTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/PersistentInstanceRegistrationTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/ProvidedDependenciesTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/ProvidedInstanceFactoryRegistrationTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/ProvidedInstanceRegistrationTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/RegistrationTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/RuntimeRegistrationTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/UseIfNoneTests.cpp.o"
+  "CMakeFiles/Hypodermic.Tests.dir/main.cpp.o"
+  "Hypodermic.Tests"
+  "Hypodermic.Tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hypodermic.Tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
